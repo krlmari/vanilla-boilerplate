173488d9d3f800c4be8b4fb643606e5fd5654b78
Vladislav Grigoriev <v1.grigoriev@yandex.ru>
2021-12-13 01:51:44 +0700

    Add prettier to ESLint

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -3,14 +3,14 @@ import { IRootComponent } from '@common/types';
 export const findComponent = <T extends Element = Element>(
     name: string
 ): IRootComponent<T> => ({
-        name,
-        node: document.querySelector(`.${name}`),
-    });
+    name,
+    node: document.querySelector(`.${name}`),
+});
 
 export const findComponents = <T extends Element = Element>(
     name: string
 ): IRootComponent<T>[] =>
-        Array.from(document.querySelectorAll(`.${name}`)).map((node: T) => ({
-            name,
-            node,
-        }));
+    Array.from(document.querySelectorAll(`.${name}`)).map((node: T) => ({
+        name,
+        node,
+    }));
