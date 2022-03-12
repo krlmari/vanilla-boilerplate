84306ba6e680a098dd1c43436f5be3a8547b8185
Vladislav Grigoriev <v1.grigoriev@yandex.ru>
2022-03-12 22:08:56 +0700

    RxJS counter

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,18 +1,10 @@
-import barba, { IViewData } from '@barba/core';
+import barba from '@barba/core';
 
 // Styles
 import '@styles/globals.scss';
 
 import pages from '@common/pages';
 
-// Common components
-import Header from '@components/header/header';
-import { findComponent } from '@common/utils';
-
 barba.init({
     views: pages,
-});
-
-barba.hooks.afterEnter((data: IViewData) => {
-    const header = new Header(findComponent('header'));
 });
---
+++ src/components/counter/counter.ts
@@ -0,0 +1,53 @@
+import RootComponent from '@common/rootComponent';
+import { IRootComponent } from '@common/types';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+
+class Counter extends RootComponent<HTMLDivElement> {
+    readonly input: HTMLInputElement;
+    readonly prevButton: HTMLButtonElement;
+    readonly nextButton: HTMLButtonElement;
+    readonly clickCount: HTMLDivElement;
+
+    public count$ = new BehaviorSubject(0);
+    public clickCount$ = new BehaviorSubject(0);
+
+    constructor(props: IRootComponent) {
+        super(props);
+
+        this.input = this.node.querySelector(`.${this.name}__value`);
+        this.prevButton = this.node.querySelector<HTMLButtonElement>(
+            `.${this.name}__button-prev`
+        );
+
+        this.nextButton = this.node.querySelector<HTMLButtonElement>(
+            `.${this.name}__button-next`
+        );
+
+        this.clickCount = this.node.querySelector(`.${this.name}__clicks`);
+
+        fromEvent([this.prevButton, this.nextButton], 'click').subscribe(
+            (target) => {
+                const button = target.target as HTMLButtonElement;
+                const isPrev = button.classList.contains(
+                    `${this.name}__button-prev`
+                );
+
+                const value = this.count$.getValue() + (isPrev ? -1 : 1);
+                this.setCount(value);
+                this.clickCount$.next(this.clickCount$.getValue() + 1);
+            }
+        );
+
+        this.count$.subscribe(
+            (target) => (this.input.value = target.toString())
+        );
+
+        this.clickCount$.subscribe(
+            (target) => (this.clickCount.innerHTML = `Clicks: ${target}`)
+        );
+    }
+
+    public setCount = (count: number) => this.count$.next(count);
+}
+
+export default Counter;
