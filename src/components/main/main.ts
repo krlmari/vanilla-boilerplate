ce0a957ff963072e73e0a0b611c3c09f455d5de5
krlmari <krylova_m_v@inbox.ru>
2022-03-16 21:14:52 +0300

    add main component

---
+++ src/components/main/main.ts
@@ -0,0 +1,10 @@
+import RootComponent from '@common/rootComponent';
+import { IRootComponent } from '@common/types';
+
+class Main extends RootComponent {
+    constructor(props: IRootComponent) {
+        super(props);
+    }
+}
+
+export default Main;
